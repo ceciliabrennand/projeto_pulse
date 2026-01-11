96507b78a5d5a0891cdf43317c8b3ffca380f9e5
Cecília <cecbrennand@gmail.com>
2026-01-11 18:32:42 -0300

    melhorei os comandos de js do menu

---
+++ script.js
@@ -0,0 +1,12 @@
+function clicar() {
+    var menuzinho = window.document.getElementById('menu')
+    if (menuzinho.style.display == 'flex') {
+    menuzinho.style.display = 'none'
+    } else {
+       menuzinho.style.display = 'flex' 
+    } 
+
+    /* 
+    */
+}
+
